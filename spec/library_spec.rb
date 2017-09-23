795a6f7a8a6935c44636a9d710e482a4a1a87342
jinco13 <jinco13@users.noreply.github.com>
2017-09-23 23:43:01 +0900

    library spec fix

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,16 +1,18 @@
 require "spec_helper"
 
 RSpec.describe CalilApi do
-  let(:library) { CalilApi::Library.new }
-  let(:expected_query) do
-    { appkey: appkey, format: 'json' }
+  
+  before do
+    allow_any_instance_of(CalilApi::Client).to receive(:appkey).and_return('lib_appkey')
   end
+
+  let(:library) { CalilApi::Library.new }
   let(:expected_response) do
     { body: '{"status":"ok"}' }
   end
   context 'get list of libraries' do
     before do
-      @expected_request = stub_request(:get, "https://api.calil.jp/library?appkey=appkey&format=json&pref=%E5%9F%BC%E7%8E%89%E7%9C%8C").
+      @expected_request = stub_request(:get, "https://api.calil.jp/library?appkey=lib_appkey&format=json&pref=%E5%9F%BC%E7%8E%89%E7%9C%8C").
          with(:headers => {'User-Agent' => "CalilApi v#{CalilApi::VERSION}(ruby-#{RUBY_VERSION} [#{RUBY_PLATFORM}])"}).
          to_return(:status => 200, :body => fixture('library.json'), :headers => {})
     end
@@ -34,7 +36,7 @@ RSpec.describe CalilApi do
 
   context 'could not get list of libraries' do
     before do
-      @expected_request = stub_request(:get, "https://api.calil.jp/library?appkey=appkey&format=json&pref=%E5%AD%98%E5%9C%A8%E3%81%97%E3%81%AA%E3%81%84%E7%9C%8C").
+      @expected_request = stub_request(:get, "https://api.calil.jp/library?appkey=lib_appkey&format=json&pref=%E5%AD%98%E5%9C%A8%E3%81%97%E3%81%AA%E3%81%84%E7%9C%8C").
          with(:headers => {'User-Agent' => "CalilApi v#{CalilApi::VERSION}(ruby-#{RUBY_VERSION} [#{RUBY_PLATFORM}])"}).
          to_return(:status => 200, :body => "[]", :headers => {})
     end
