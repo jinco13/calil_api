af4c1871fb2b0a525b92471bdf8de6d8ba1b2cfc
jinco13 <jinco13@users.noreply.github.com>
2017-09-23 23:24:46 +0900

    calil_api

--- lib/calil_api.rb
+++ lib/calil_api.rb
@@ -1,4 +1,6 @@
 require "calil_api/version"
+require "calil_api/client"
+require "calil_api/library"
 
 module CalilApi
   # Your code goes here...
--- spec/calil_api_spec.rb
+++ spec/calil_api_spec.rb
@@ -1,11 +1,8 @@
 require "spec_helper"
 
 RSpec.describe CalilApi do
+  let(:endpoint){ 'https://dummy.api.com' }
   it "has a version number" do
     expect(CalilApi::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
