f404978199056c2b4a030f790d4130c2977c350e
jinco13 <jinco13@users.noreply.github.com>
2017-10-12 04:53:21 +0900

    made retry waiting time 5 seconds to 2 seconds

--- lib/calil_api/configuration.rb
+++ lib/calil_api/configuration.rb
@@ -5,8 +5,10 @@ module CalilApi
     URL_BOOK = "https://api.calil.jp/check"
     URL_LIBRARY = "https://api.calil.jp/library"
 
-    RETRY_LIMIT = 10
-    RETRY_WAIT_TIME = 5
+    RETRY_LIMIT = 15
+    # http://calil.jp/doc/api_ref.html
+    # ポーリングは、かならず2秒以上の間隔をあけて呼び出してください
+    RETRY_WAIT_TIME = 2
 
     STATUS_LOADING = 1
     STATUS_FINISHED = 0
